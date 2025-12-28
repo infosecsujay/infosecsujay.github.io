4106ad32b23a12d74edfd5f70c5958cde210badc
infosecsujay <infosecsujay@gmail.com>
2025-12-29 02:12:02 +0530

    Fix GitHub Pages paths by nesting under /ss

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
-const repo = 'ss';
-
 const nextConfig = {
   output: 'export',
-
-  basePath: `/${repo}`,
   trailingSlash: true,
-
-  distDir: 'docs', // 🔑 THIS IS THE KEY FIX
 
   images: {
     unoptimized: true,
