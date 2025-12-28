566f3db9757ad996a0c1db658929d7c497c9eb7b
infosecsujay <infosecsujay@gmail.com>
2025-12-29 01:38:45 +0530

    Use docs folder for GitHub Pages

---
+++ docs/_next/static/chunks/app/layout-d39247f464326d74.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{8607:function(n,e,u){Promise.resolve().then(u.t.bind(u,9714,23))},9714:function(){}},function(n){n.O(0,[487,971,23,744],function(){return n(n.s=8607)}),_N_E=n.O()}]);
